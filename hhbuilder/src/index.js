151dff1042d58abdb9443974ba23efdde0a82c92
blah blah <sa-mm@users.noreply.github.com>
2017-03-03 15:20:03 -0600

    MVC it

--- hhbuilder/src/index.js
+++ hhbuilder/src/index.js
@@ -1,289 +1,330 @@
-var age,
-  rel,
-  smoker,
-  addBtn,
-  submitBtn,
-  form,
-  buttons,
-  householdList;
+var FormView = function () {
+  var age,
+    rel,
+    smoker,
+    addBtn,
+    submitBtn,
+    form,
+    buttons,
+    debug,
+    household;
 
-form = document.forms[0];
-age = form.elements.age;
-rel = form.elements.rel;
-smoker = form.elements.smoker;
-addBtn = document.getElementsByClassName('add')[0];
-//attempting to get the submit button via a unique attribute
-buttons = document.getElementsByTagName('button');
-for (var i = 0; i < buttons.length; i++) {
-  if (buttons[i].textContent.toLowerCase() === 'submit') {
-    submitBtn = buttons[i];
+  form = document.forms[0];
+  age = form.elements.age;
+  rel = form.elements.rel;
+  smoker = form.elements.smoker;
+  addBtn = document.getElementsByClassName('add')[0];
+  //attempting to get the submit button via a unique attribute
+  buttons = document.getElementsByTagName('button');
+  for (var i = 0; i < buttons.length; i++) {
+    if (buttons[i].textContent.toLowerCase() === 'submit') {
+      submitBtn = buttons[i];
+    }
+  }
+  debug = document.getElementsByClassName('debug')[0];
+  household = document.getElementsByClassName('household')[0];
+
+  return {
+    age: age,
+    rel: rel,
+    smoker: smoker,
+    addBtn: addBtn,
+    buttons: buttons,
+    submitBtn: submitBtn,
+    debug: debug,
+    household: household
   }
 }
-householdList = []; //where we store the members added to the household
 
+var FormModel = function () {
+  var members = [];
 
-addBtn.addEventListener('click', addMember, false);
-submitBtn.addEventListener('click', submitHousehold, false);
-
-// addBtn event listener callback
-function addMember(event) {
-  var member,
-    data,
-    errors;
-
-  event.preventDefault();
-
-  member = {
-    'age': age.value, //string
-    'rel': rel.value, //string
-    'smoker': smoker.checked //boolean
-  };
-
-  /// data = { 'isValid': false, 'errors': [{'type': 'age', 'msg': "Error…" }]}
-  clearErrors();
-  data = validateData(member);
-  if (data.isValid) {
-    addToHousehold(member);
-    createHTMLList();
-    resetForm();
-  } else {
-    errors = data.errors;
-    errors.forEach(function (error) {
-      displayErrorMessage(error.element, error.msg);
-    });
+  function getMembers() {
+    return members;
   }
-};
 
-// This allows for rather open relationships:
-// Multiple spouses, children older than parents, etc.
-var validateData = function (member) {
-  var result,
-    ageError,
-    relError;
+  function addMember(member) {
+    members.push(member);
+  }
 
-  result = {};
-  result.isValid = true;
-  result.errors = [];
-
-  // age is required and must be greater than 0
-  if (!+member.age > 0 || member.age === '') {
-    result.isValid = false;
-    result.errors.push({
-      'element': age,
-      'msg': 'Please enter an age greater than 0.'
+  function removeMember(member) {
+    members = members.filter(function (e) {
+      return !isEqual(member, e);
     });
   }
 
-  // relationship is required
-  if (member.rel === '') {
-    result.isValid = false;
-    result.errors.push({
-      'element': rel,
-      'msg': 'Please select a relationship.'
-    });
-  }
+  function isEqual(obj1, obj2) {
+    var obj1Props,
+      obj2Props;
 
-  // Cannot enter two people with relationship "self"
-  if (member.rel === 'self' && checkSelfRel(member.rel)) {
-    result.isValid = false;
-    result.errors.push({
-      'element': rel,
-      'msg': 'Please enter yourself only once.'
-    });
-  }
-  return result;
-}
-
-// just a quick function to help with validation 
-var checkSelfRel = function (rel) {
-  var self;
-
-  self = householdList.filter(function (member) {
-    return member.rel === rel;
-  });
-
-  if (self.length > 0) {
-    return true; //There can't be two selves.
-  }
-  return false;
-}
-
-
-
-//display an error message near the relevant element
-var displayErrorMessage = function (element, msgText) {
-  var elementError,
-    msg;
-
-  elementError = document.createElement('p');
-  msg = document.createTextNode(msgText);
-  elementError.appendChild(msg);
-  elementError.className = 'formError';
-  elementError.style.color = 'red';
-  div = element.parentNode;
-  div.appendChild(elementError);
-}
-
-// clear any formError errors
-var clearErrors = function () {
-  var errors;
-
-  errors = document.getElementsByClassName('formError');
-  while (errors.length > 0) {
-    var child;
-
-    child = errors[0];
-    child.parentNode.removeChild(child);
-  }
-}
-
-var addToHousehold = function (member) {
-  householdList.push(member);
-}
-
-// creates the list of members
-var createHTMLList = function () {
-  var householdEl;
-
-  //just rebuild the HTML list from scratch every time.
-  householdEl = document.getElementsByClassName('household')[0];
-  clearElement(householdEl);
-
-  //add every item in the householdList array to the
-  //household ol element as a child;
-  for (var i = 0; i < householdList.length; i++) {
-    var member,
-      li,
-      memberText,
-      removeBtn;
-
-    member = householdList[i];
-    li = document.createElement("LI");
-    memberText = document.createTextNode(JSON.stringify(member));
-    removeBtn = createBtn("remove");
-    li.appendChild(memberText);
-    li.insertAdjacentElement('beforeend', removeBtn);
-    householdEl.appendChild(li);
-    removeBtn.addEventListener('click', removeItem, false);
-  }
-}
-
-//resets form to default values
-var resetForm = function () {
-  age.value = "";
-  rel.value = "";
-  smoker.checked = false;
-}
-
-//creates remove/edit buttons as necessary
-var createBtn = function (textStr) {
-  var button,
-    text;
-
-  button = document.createElement("BUTTON");
-  text = document.createTextNode(textStr);
-  button.appendChild(text);
-  button.className = textStr.split(' ')[0];
-  return button;
-}
-
-// remove item from html list
-var removeItem = function () {
-  var li;
-
-  li = event.target.parentNode;
-  li.parentNode.removeChild(li);
-  removeFromHousehold(JSON.parse(li.firstChild.data));
-  event.target.removeEventListener('click', removeItem, false);
-}
-
-// remove item from householdList array
-var removeFromHousehold = function (member) {
-  householdList = householdList.filter(function (item) {
-    return !isEqual(member, item);
-  });
-}
-
-// checks that two objects are equivalent (shallow)
-var isEqual = function (obj1, obj2) {
-  var obj1Props,
-    obj2Props;
-
-  obj1Props = Object.getOwnPropertyNames(obj1);
-  obj2Props = Object.getOwnPropertyNames(obj2);
-
-  if (obj1Props.length !== obj2Props.length) {
-    return false;
-  }
-
-  for (var i = 0; i < obj1Props.length; i++) {
-    var propName;
-
-    propName = obj1Props[i];
-    if (obj1[propName] !== obj2[propName]) {
+    obj1Props = Object.getOwnPropertyNames(obj1);
+    obj2Props = Object.getOwnPropertyNames(obj2);
+    if (obj1Props.length !== obj2Props.length) {
       return false;
     }
+
+    for (var i = 0; i < obj1Props.length; i++) {
+      var propName;
+
+      propName = obj1Props[i];
+      if (obj1[propName] !== obj2[propName]) {
+        return false;
+      }
+    }
+    return true;
   }
 
-  return true;
+  return {
+    getMembers: getMembers,
+    addMember: addMember,
+    removeMember: removeMember
+  }
 }
 
-//submit event listener callback
-function submitHousehold(event) {
-  var debug,
-    householdText,
-    editBtn,
-    householdEl;
+var FormController = function (pModel) {
+  var model,
+    view,
+    member;
 
-  event.preventDefault();
-  debug = document.getElementsByClassName('debug')[0];
-  editBtn = createBtn("edit submission");
-  householdText = document.createTextNode(JSON.stringify(householdList, null, '\t'));
-  householdEl = document.getElementsByClassName('household')[0];
+  model = new FormModel();
+  view = new FormView();
 
-  clearErrors();
-  if (householdList.length === 0) {
-    displayErrorMessage(event.target, "Please add at least one member to your household.");
-    return;
+  function addAndUpdate(event) {
+    var validatedMember;
+
+    event.preventDefault();
+    // build member from form
+    member = {
+      age: view.age.value,
+      rel: view.rel.value,
+      smoker: view.smoker.checked
+    }
+
+    //clear warnings
+    clearErrors();
+
+    //validate member
+    validatedMember = validateMember(member);
+    if (validatedMember.hasError) {
+      return false;
+    }
+
+    //insert member into model
+    model.addMember(member);
+
+    // update view based on model
+    createList();
+    resetForm();
   }
 
-  if (debug.previousSibling.textContent === 'edit submission') {
-    clearElement(debug);
-    clearElement(debug.previousSibling);
+  function createSubmission(event) {
+    var members,
+      debug,
+      household,
+      confirmation,
+      buttons,
+      editBtn;
+
+    event.preventDefault();
+    // check that there are members to submit
+    members = model.getMembers();
+    clearErrors();
+    if (members.length === 0) {
+      displayError(view.submitBtn, 'Please add one member to the household before submitting.');
+      return false;
+    }
+
+    // submit members
+    debug = view.debug;
+    debug.textContent = JSON.stringify(members, null, '\t');
+    debug.style.display = 'block';
+
+    //disable buttons
+    household = view.household;
+    confirmation = makeBuilderElement('P', "Submitted.");
+    household.insertAdjacentElement('afterend', confirmation);
+    buttons = view.buttons;
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      button.disabled = true;
+    }
+    editBtn = makeBuilderElement('BUTTON', 'edit submission');
+    household.insertAdjacentElement('afterend', editBtn);
+    editBtn.addEventListener('click', editSubmission, false);
   }
 
-  debug.appendChild(householdText);
-  debug.insertAdjacentElement('beforebegin', editBtn);
-  debug.style.display = 'block';
-  clearElement(householdEl);
-  householdList = [];
-  editBtn.addEventListener('click', editSubmission, false);
-}
+  function removeAndUpdate(event) {
+    var member,
+      re;
 
-function editSubmission(event) {
-  var debug,
-    previousSubmission;
+    event.preventDefault();
+    // get data from associated html list
+    member = makeMember(event.target.parentNode.firstElementChild);
 
-  debug = document.getElementsByClassName('debug')[0];
-  previousSubmission = debug.textContent;
+    //remove item from model.
+    model.removeMember(member);
 
-  event.preventDefault();
+    //update html list
+    createList();
+    document.removeEventListener('click', removeAndUpdate);
+  }
 
-  householdList = JSON.parse(previousSubmission);
-  createHTMLList();
-  clearElement(debug);
-  clearElement(debug.previousSibling);
-  debug.style.display = 'none';
-  event.target.removeEventListener('click', editSubmission, false);
-}
+  function editSubmission(event) {
+    var debug,
+      members,
+      buttons;
 
-var clearElement = function (el) {
-  if (el.tagName === 'BUTTON') {
-    el.parentNode.removeChild(el);
-  } else {
-    while (el.firstChild) {
-      el.removeChild(el.firstChild);
+    event.preventDefault();
+
+    // update html list
+    createList();
+
+    // reset debug
+    debug = view.debug;
+    debug.textContent = '';
+    debug.style.display = 'none';
+
+    //remove edit submission button and confirmation
+    event.target.parentNode.removeChild(event.target.nextElementSibling);
+    event.target.parentNode.removeChild(event.target);
+
+    // enable buttons that were disabled upon submission
+    buttons = view.buttons;
+    for (var i = 0; i < buttons.length; i++) {
+      var button = buttons[i];
+      button.disabled = false;
+    }
+    document.removeEventListener('click', editSubmission);
+  }
+
+  function displayError(element, msg) {
+    var error,
+      div;
+
+    error = makeBuilderElement('P', msg);
+    error.style.color = 'red';
+    error.className = 'error';
+    div = element.parentNode;
+    div.appendChild(error);
+  }
+
+  function clearErrors() {
+    var errors = document.getElementsByClassName('error');
+    while (errors.length > 0) {
+      var error = errors[0];
+      error.parentNode.removeChild(error);
     }
   }
+
+  function validateMember(member) {
+    var hasError,
+      rels;
+
+    hasError = false;
+    if (!+member.age > 0 || member.age === '') {
+      displayError(view.age, 'Please enter an age greater than 0.');
+      hasError = true;
+    }
+    if (member.rel === '') {
+      displayError(view.rel, 'Please select a relationship.')
+      hasError = true;
+    }
+    // check if "self" has already been entered
+    if (member.rel === 'self') {
+      rels = model.getMembers().map(function (e) {
+        return e.rel;
+      });
+      if (rels.includes(member.rel)) {
+        displayError(view.rel, 'Please only enter yourself once.');
+        hasError = true;
+      }
+    }
+
+    if (hasError) {
+      member.hasError = true;
+    }
+    return member;
+  }
+
+  function createList() {
+    var members,
+      household;
+
+    members = model.getMembers();
+    household = view.household;
+    while (household.firstChild) {
+      household.removeChild(household.firstChild);
+    }
+    for (var i = 0; i < members.length; i++) {
+      var member,
+        re,
+        memberText,
+        pre,
+        li,
+        removeBtn;
+
+      member = members[i];
+      re = /[{}"]/g;
+      memberText = JSON.stringify(member, null, '\t').replace(re, '');
+      pre = makeBuilderElement('PRE', memberText);
+      li = makeBuilderElement('LI', '');
+      removeBtn = makeBuilderElement('BUTTON', 'remove');
+      li.appendChild(pre);
+      li.appendChild(removeBtn);
+      household.appendChild(li);
+
+      removeBtn.addEventListener('click', removeAndUpdate, false);
+    }
+  }
+
+  function createMemberText(member) {
+    var age,
+      rel,
+      smoker,
+      text;
+
+    age = member.age;
+    rel = member.rel;
+    smoker = member.smoker;
+    text = 'Age: ' + age + ',\n' + 'Relationship: ' + rel + ', ' + 'Smoker: ' + smoker;
+    return text;
+  }
+
+  function makeMember(item) {
+    var member,
+      re;
+
+    member = "{" + item.textContent.trim() + "}";
+    re = /\w+/g;
+    member = member.replace(re, function (word) {
+      if (word === 'false' || word === 'true') {
+        return word;
+      }
+      return '"' + word + '"';
+    });
+    member = JSON.parse(member);
+    return member;
+  }
+
+  function makeBuilderElement(el, text) {
+    var el,
+      textNode;
+
+    el = document.createElement(el);
+    textNode = document.createTextNode(text);
+    el.appendChild(textNode);
+    return el;
+  }
+
+  function resetForm() {
+    view.age.value = '';
+    view.rel.value = '';
+    view.smoker.checked = false;
+  }
+
+  view.addBtn.addEventListener('click', addAndUpdate, false);
+  view.submitBtn.addEventListener('click', createSubmission, false);
 }
+
+new FormController();
 
